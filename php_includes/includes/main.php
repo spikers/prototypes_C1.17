60133df3816206441462e187c49705e7f8556983
spikers <timfengineering@gmail.com>
2017-02-15 12:19:51 -0800

    PHP Includes - Tim Feng

---
+++ php_includes/includes/main.php
@@ -0,0 +1,3 @@
+<main id="main_content">
+  Main Content
+</main>
---
+++ php_includes/index_main.php
@@ -0,0 +1,3 @@
+<?php include("includes/main.php");?>
+<div>I'm not in the main content or the footer</div>
+<?php include("includes/footer.php");?>
