d101ac428f9538855088638697f8cdd90e3ed08a
spikers <timfengineering@gmail.com>
2017-03-02 20:28:11 -0800

    Firebase Intro - Tim Feng

--- js_firebase_intro/hard.js
+++ js_firebase_intro/hard.js
@@ -1,10 +1,58 @@
 // harder Firebase Intro Prototype js file
+var config = {
+    apiKey: "AIzaSyDZXytaLOvVWUsLWztNwyenEpLMPKigFm4",
+    authDomain: "lfz-student-database.firebaseapp.com",
+    databaseURL: "https://lfz-student-database.firebaseio.com",
+    storageBucket: "lfz-student-database.appspot.com",
+    messagingSenderId: "344869651480"
+};
+
+firebase.initializeApp(config);
+
+var dbRef = firebase.database();
+var students = dbRef.ref('Students');
+
+students.on('value', function(snapshot) {
+    updateDom(snapshot.val());
+});
+
+
+window.addEventListener('load', function() {
+    var addStudentButton = document.getElementById('add-student');
+    addStudentButton.addEventListener('click', handleAddButtonClick)
+    //var deleteStudentBUtton = document.getElementById('')
+    var clearButton = document.getElementById('clear-form');
+    clearButton.addEventListener('click', clearForm);
+});
+
+function handleAddButtonClick() {
+    //students.push(formInfo);
+    var newStudent = {};
+
+    newStudent = getFormattedFormData();
+    students.push(newStudent);
+
+    clearForm();
+    console.log(newStudent);
+
+} 
+
+function getFormattedFormData() {
+    var formInfo = getFormData();
+    var newStudent = {};
+    newStudent['student_name'] = formInfo.sname;
+    newStudent['student_id'] = formInfo.sid;
+    newStudent['grade'] = formInfo.grade;
+    newStudent['course'] = formInfo.course;
+    return newStudent;
+}
 
 function updateDom(d){
     var table = $('.sgt tbody');
     table.html('');
     for(var key in d){
         if(d.hasOwnProperty(key)){
+            (function() {
             var row = $('<tr>');
             var id = $('<td class="sid">' + d[key].student_id + '</td>');
             var name = $('<td class="sname">' + d[key].student_name + '</td>');
@@ -20,7 +68,23 @@ function updateDom(d){
                 text: 'Delete'
             });
 
+            var closureKey = key;
+
+            del.on('click', () => {
+                dbRef.ref('Students/' + closureKey).remove();
+                console.log(closureKey);
+            });
+
+            edit.on('click', () => {
+                var updates = {};
+                var formData = getFormattedFormData();
+                updates['Students/' + closureKey] = formData;
+                dbRef.ref().update(updates);
+                clearForm();
+            });
+
             table.append(row.append(id, name, course, grade, actions.append(edit, del)));
+        }());
         }
     }
 }
