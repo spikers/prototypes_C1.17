123d96602c04d03f9bee78be560eac57b98f5a9c
spikers <timfengineering@gmail.com>
2017-03-02 21:25:14 -0800

    Angular Routes - Tim Feng

--- ang_routes/script.js
+++ ang_routes/script.js
@@ -1,7 +1,25 @@
 // Create the route module and name it routeApp
+var app = angular.module('routeApp', ['ngRoute']).controller('routeCtrl', function() {
+  
+});
 
+app.config(function($routeProvider) {
 // Config the routes
-
+  $routeProvider.when('/', {
+    templateUrl: './pages/home.html',
+    controller: 'homeController',
+    controllerAs: 'hc'
+  }).when('/contact', {
+    templateUrl: './pages/contact.html',
+    controller: 'contactController',
+    controllerAs: 'cc'
+  }).when('/about', {
+    templateUrl: './pages/about.html',
+    controller: 'aboutController',
+    controllerAs: 'ac'
+  }).otherwise({
+    redirectTo: '/'
+  });
          // route for the home page
 
         // route for the about page
@@ -9,14 +27,26 @@
         // route for the contact page
 
 
+
+});
+
+
 // Create the controllers for the different pages below
 
 // home page controller
-    // Create a message to display in the view
-
-
+// Create a message to display in the view
+app.controller('homeController', function() {
+  this.message = 'Home Controller';
+});
 // about page controller
-    // Create a message to display in the view
+// Create a message to display in the view
+app.controller('aboutController', function() {
+  this.message = 'About Controller';
+});
 
 // contact page controller
-    // Create a message to display in the view
+// Create a message to display in the view
+
+app.controller('contactController', function() {
+  this.message = 'Contact Controller';
+});
