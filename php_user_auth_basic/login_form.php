eff5a7be28b78d1b535f63037d2f84e6cc904453
spikers <timfengineering@gmail.com>
2017-03-05 22:54:06 -0800

    PHP User Auth - Tim Feng

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,51 @@
 <!--login_form.php-->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Form</title>
+  <script>
+  window.onload = function() {
+    let form = document.getElementById('form')
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      submitData();
+    });
+  };
+
+  function submitData() {
+    let user = document.getElementById('username').value;
+    let pass = document.getElementById('password').value;
+    let formData = new FormData();
+
+    formData.append('username', user);
+    formData.append('password', pass);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', (data) => {
+      console.log(data.target.response);
+      let loginStatus = document.getElementById('login_status');
+      let tn = document.createTextNode(data.target.response.message);
+
+      loginStatus.appendChild(tn);
+
+    });
+
+
+    xhr.open('POST', 'login_handler.php');
+
+    xhr.send(formData);
+  }
+  </script>
+</head>
+<body>
+  <form id="form">
+    Username: <input id="username" type="text">
+    Password: <input id="password" type="password">
+    <button id="submit">Submit</button>
+  </form>
+  <p id="login_status"></p>
+</body>
+</html>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,42 @@
-<!--login_handler.php-->
+<?php
+session_start();
+$_POST['password'] = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$output = [
+  'success'=>['success'=>true, 'message'=>'Welcome, '.$_POST['username']],
+  'error'=>['success'=>false, 'message'=>'User or Password Not Found'],
+];
+
+//Okay I did this way beforehand, so it's not the SHA1 encryption.*/
+$user_info = [
+  ['id'=> 0, 'username'=>'tim', 'password'=>'$2y$10$UkU79O3KAoizg.X8.gIxRO2VseyO/ADLHmAmEg5Eu6eWfuJkBhzBC'],
+  ['id'=> 1, 'username'=>'danny', 'password'=>'$2y$10$3FHU1qS18IgMvl6wq2tAIOXTSMFAyUnYwmymUh4OreCVfS9q8dGJa'],
+  ['id'=> 2, 'username'=>'gwen', 'password'=>'$2y$10$rCBdotVNj4iW8.qGBIvSrugWuqeYTrs31qPIKVXVKPuSZ6KMLDnIa'],
+  ['id'=> 3, 'username'=>'mike', 'password'=>'$2y$10$MV/qgERXv5F4Zvt6qH1H5ObRaC5G9NbxmVLKvJxn1uEgWZwL1y1dW'],
+  ['id'=> 4, 'username'=>'mark', 'password'=>'$2y$10$Ox/Ez7EgrLf2dHqykgbFG.wnl57GIE6Ez2oWUtQK3pyQKUuNeVgeG']
+];
+
+//Password is: securetim, or securemark, etc
+
+$i = 0;
+$match = -1;
+while ($i < sizeof($user_info)) {
+  if ($user_info[$i]['username'] === $_POST['username']) {
+    $outputString = json_encode($output['success']);
+
+    print($outputString);
+    $match = $i;
+    $_SESSION['user_id'] = $_POST['username'];
+    $_POST = '';
+    return;
+  }
+  $i = $i + 1;
+}
+$_POST = '';
+
+$outputString = json_encode($output['error']);
+print($outputString);
+
+
+
+?>
